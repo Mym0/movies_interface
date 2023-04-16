13af74d68de370fc2d5a5cf9f64d990c695cee41
Mohamed Elgendy <memoelgendy40@gmail.com>
2023-04-16 15:04:56 +0200

    create Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVideoSlash } from '@fortawesome/free-solid-svg-icons';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="/" style={{ color: 'gold' }}>
+          <FontAwesomeIcon icon={faVideoSlash} />
+          Gold
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/watchList">
+              Watch List
+            </NavLink>
+          </Nav>
+          <Link to='/login' className="me-5 link-gradient">
+            Login
+          </Link>
+          <Link to='/register' className='link-gradient'>Register</Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
